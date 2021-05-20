8e2536f6756182b2f1d343ff224c91a96f4950a0
sébastien jacq <57099003+sjthales@users.noreply.github.com>
2021-05-20 14:14:46 +0200

    Update irqflags.h

--- arch/riscv/include/asm/irqflags.h
+++ arch/riscv/include/asm/irqflags.h
@@ -18,6 +18,7 @@
 #include <asm/processor.h>
 #include <asm/csr.h>
 #include <linux/kernel.h>
+#include <linux/printk.h>
 
 /* read interrupt enabled status */
 static inline unsigned long arch_local_save_flags(void)
